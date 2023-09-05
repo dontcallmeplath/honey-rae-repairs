0c4af188dfc46c2400ca8a6bc4bff20a7c30c918
Sylvia T <sylviarosemarie@gmail.com>
2023-09-05 16:13:10 -0500

    Began process of modularizing code following Val's tutorial

---
+++ src/components/tickets/TicketList.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { getAllTickets } from "../../services/TicketService";
+import { Ticket } from "./Ticket";
+import "./Tickets.css";
+
+export const TicketList = () => {
+  const [allTickets, setAllTickets] = useState([]);
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
+  const [filteredTickets, setFilteredTickets] = useState([]);
+
+  useEffect(() => {
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray);
+      console.log("tickets set !");
+    });
+  }, []);
+
+  useEffect(() => {
+    if (showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      );
+      setFilteredTickets(emergencyTickets);
+    } else {
+      setFilteredTickets(allTickets);
+    }
+  }, [showEmergencyOnly, allTickets]);
+
+  return (
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <div>
+        <button
+          className="filter-btn btn-primary"
+          onClick={() => {
+            setShowEmergencyOnly(true);
+          }}
+        >
+          Emergency
+        </button>
+        <button
+          className="filter-btn btn-info"
+          onClick={() => {
+            setShowEmergencyOnly(false);
+          }}
+        >
+          Show All
+        </button>
+      </div>
+      <article className="tickets">
+        {filteredTickets.map((ticketObj) => {
+          return <Ticket ticket={ticketObj} key={ticketObj.id} />;
+        })}
+      </article>
+    </div>
+  );
+};
